3b11b1db590177df08f04c4989b8706054729128
BlakeDemonvine <blakekaidemonvine@gmail.com>
2025-07-05 17:12:58 +0800

    Update control.js

--- control.js
+++ control.js
@@ -1,10 +1,14 @@
 let user = null;
 const password = {
-  'tommy' : '0705'
+  'tommy' : '0705',
+  'mcWhite0863' : 'Jerry1108'
 };
 
 const userInfo = {
   'tommy' : {
     'ac' : ['A001','B001']
+  },
+  'mcWhite0863' : {
+    'ac' : ['A001','A002','A005','A006','E002']
   }
 }
