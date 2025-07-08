263d23a4a84564f73afd46dab9e66b4f820817c3
BlakeDemonvine <blakekaidemonvine@gmail.com>
2025-07-08 12:26:51 +0800

    Update control.js

--- control.js
+++ control.js
@@ -25,12 +25,12 @@ const userInfo = {
       'B001' : {
         'correction':false, 
         'description':'程式錯誤之處為：題目敘述【輸入一行文字】，而不是請你宣告，因此應使用輸入語法【名稱 = input()】來定義str變數(字串)，最後使用print語法輸出。要注意的是，輸出時由於是輸出【變數】，因此不需要使用單引號或是雙引號將其包住喔。<br>(有問題可以使用表單打下你的問題，沒有問題看懂的話請記得在下次上課前再次繳交改正的程式碼，重新繳交的程式碼不能和範例程式碼相同，要自己再寫一遍。)',
-        'samplesCode': 'name = input\nprint(name)'
+        'samplesCode': 'name = input()\nprint(name)'
       },
       'D001' : {
         'correction':false, 
         'description':'程式錯誤之處和B001相同：題目敘述【輸入一個字串】，因此應使用輸入語法【名稱 = input()】來定義str變數。你有正確使用len函式去計算字串的長度非常好，一點小小的問題再改更一下。<br>(有問題可以使用表單打下你的問題，沒有問題看懂的話請記得在下次上課前再次繳交改正的程式碼，重新繳交的程式碼不能和範例程式碼相同，要自己再寫一遍。)',
-        'samplesCode': 'name = input\nprint(len(name))'
+        'samplesCode': 'name = input()\nprint(len(name))'
       },
       
     }
