d3173b5a1a9c33f31f2ab0b1d2d970f1eaedac04
BlakeDemonvine <blakekaidemonvine@gmail.com>
2025-07-19 20:19:09 +0800

    Update control.js

--- control.js
+++ control.js
@@ -16,7 +16,7 @@ const userInfo = {
     'message' : []
   },
   'Brian' : {
-    'ac' : ['A001','B001','D001'],
+    'ac' : ['A001','A011','B001','B002','D001'],
     'message' : {
       'B001' : {
         'correction':true,
